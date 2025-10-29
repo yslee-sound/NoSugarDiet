79bbb2a957f141f5a0fff6f51e67602381f949b9
ryan <sweetplastik@gmail.com>
2025-10-29 19:10:17 +0900

    Release_1.0.2.00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
 
     // 버전 코드 전략: yyyymmdd + 2자리 시퀀스 (NN)
     // 이전 사용: 2025100800 -> 신규: 2025100801
-    val releaseVersionCode = 2025102700
-    val releaseVersionName = "1.0.1"
+    val releaseVersionCode = 2025102900
+    val releaseVersionName = "1.0.2"
 
     defaultConfig {
         applicationId = "com.sweetapps.nosugardiet" // 변경: Play Console에서 com.example.* 금지 대응
